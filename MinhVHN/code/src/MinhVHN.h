6c6e70f1856b57cfc6f420ffed00d07b268f120a
VuHuyNhatMinh <nminhstickpk@gmail.com>
2022-03-26 14:09:37 +0700

    Comment unnecessary code

--- MinhVHN/code/src/MinhVHN.h
+++ MinhVHN/code/src/MinhVHN.h
@@ -27,7 +27,7 @@ void readTCS()
     Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
     Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
     Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+    // Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
     Serial.println(" ");
 }
 
