d556211038d1264caecbdce7c816988126dc237a
Quân Đinh <you@example.com>
2022-04-24 21:43:58 +0700

    add GET, POST methods to REST API, but something still goes wrong

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # TODO: GET, POST method - Dinh Hong Quan
+    path('listCreate/',views.SensorListCreate.as_view()),
 
     # PUT/PATCH method
     path('update/<int:pk>', views.SensorUpdate.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,10 +1,19 @@
 from codecs import lookup_error
 from rest_framework import generics
+from yaml import serialize
 
 from sensors.models import Sensors
 from sensors.serializers import SensorSerializer
 
 # TODO: GET, POST method - Dinh Hong Quan
+
+class SensorListCreate(generics.ListCreateAPIView):     #hình như cái này là cả Create và cả GET toàn bộ records về luôn
+    queryset = Sensors.objects.all()
+    serializer_class = SensorSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+        
 
 class SensorUpdate(generics.UpdateAPIView):
     queryset = Sensors.objects.all()
